59f0ec5643324a7ccd4425b562495ce3c5d49ebf
Admin <Admin@DESKTOP-VEHROSA>
2020-11-02 18:14:42 +0530

    2nd nov

---
+++ js/dom1.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function(){
+    console.log("windows: " + window);
+    console.log("document: " + document);
+    console.log("document.head: " + document.head);
+    console.log("document.title: " + document.title);
+    console.log("document.body" + document.body);
+});
+
+$(document).ready(function(){
+    console.log("windows: " + window);
+    console.log("document: " + document);
+    console.log("document.head: " + document.head);
+    console.log("document.title: " + document.title);
+    console.log("document.body" + document.body);
+});
